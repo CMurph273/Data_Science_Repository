28a43532c38dfc2594c63f5e378394a32cdb4e92
CMurph273 <115643538+CMurph273@users.noreply.github.com>
2023-03-18 15:00:47 -0400

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1>Data Science Tools And Ecosystem</H1>
+
+# This notebook is a summary of data science and ecosystem.
+
+# <b>Objectives:<b><br>
+#     
+#     -List popular programming languages used by data scientists<br>
+#     -List popular data science tools used by data scientists<br>
+#     -List popular data science libraries used by data scientists<br>
+#     -Perform math operations using Python code.
+
+# Some of the popular languages used by data scientists include:<br>
+# 1.Python<br>
+# 2.R<br>
+# 3.SQL<br>
+# 4.Scala<br>
+# 5.Swift
+
+# A few common libraries used by data scientists include:<br>
+# 1.TensorFlow<br>
+# 2.PyTorch<br>
+# 3.scikit-learn
+
+# |<b>Data Science Tools<b>|
+# |:-----------------------|
+# |SAS                     |
+# |Apache Spark            |
+# |MATLAB                  |
+
+# <H3>Here are a few math operations you can perform with Python</H3>
+
+# In[3]:
+
+
+#This is how you can multiply and add numbers.
+(3*4)+5
+
+
+# In[14]:
+
+
+#This cell will convert 200 minutes into hours.
+print("200 minutes is the same as", 200/60, "hours")
+
+
+# <H2>Author</H2><br>
+# Christopher Murphy
+
+# In[ ]:
+
+
+
+
